15bf89652a2c531ba2904e87ba2c1289d05e68ab
Maelle81 <maelle.rampnoux@etud.univ-jfc.fr>
2022-02-10 14:17:48 +0100

    Push

--- src/main/java/monprojet/controller/HelloController.java
+++ src/main/java/monprojet/controller/HelloController.java
@@ -1,5 +1,7 @@
 package monprojet.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,15 +13,28 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import lombok.extern.slf4j.Slf4j;
+import monprojet.dao.CityRepository;
+import monprojet.entity.City;
 
 @Controller // This means that this class is a Controller
 @RequestMapping(path = "/hello") // This means URL's start with /hello (after Application path)
 @Slf4j
 public class HelloController {
 	
+	//On injecte le repository 
+	@Autowired
+	CityRepository dao;
 	// On affichera par défaut la page 'hello.html'
 	private static final String DEFAULT_VIEW = "hello";
 	
+	@GetMapping(path="villes")
+	public String afficheLesVilles(Model model){
+		List<City> cities = dao.findAll();
+		model.addAttribute("message", "World");
+		model.addAttribute("cities", cities);
+		return DEFAULT_VIEW;
+	}
+
 	@GetMapping(path = "noParam")
 	public String sayHello(Model model) {
 		model.addAttribute("message", "World");
@@ -31,6 +46,7 @@ public class HelloController {
 	public String sayHelloTo(
 		@RequestParam(required = false) String name, 
 		Model model
+		//http://localhost:8989/hello/Param?name='toto'
 	) {
 		model.addAttribute("message", name);
 		return DEFAULT_VIEW;
--- src/main/java/monprojet/dao/CityRepository.java
+++ src/main/java/monprojet/dao/CityRepository.java
@@ -13,4 +13,6 @@ public interface CityRepository extends JpaRepository<City, Integer> {
      */
     public City findByName(String cityName);
 
+    
+
 }
